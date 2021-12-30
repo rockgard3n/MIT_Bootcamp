3ef80e826f799703d69acb2e25996edf3fbaf0b8
rockgard3n <90724599+rockgard3n@users.noreply.github.com>
2021-12-30 12:10:01 -0500

    Using GEOJSON
    using GEOJSON method in the BusObject arrays

--- Week9/SFMTA_Track/SFmapmarker.js
+++ Week9/SFMTA_Track/SFmapmarker.js
@@ -1,4 +1,5 @@
 // this picks the bus route you want to mess with
+// ive been blocked by the SFMTA, becuase i had a loop inside the createmap fnction that i thought was only hitting every 15 seconds but was calling the.onload function by accident so was spamming the API
 const selectedRoute = 1;
 
 // const url = "http://api.511.org/transit/VehicleMonitoring?api_key=f0e7e4f5-928b-4378-abdd-2f04c0c4d0ae&agency=SF"
@@ -7,7 +8,7 @@ function createMap() {
     // TODO: add your own access token
     mapboxgl.accessToken = 'pk.eyJ1Ijoicm9ja2dhcmQzbiIsImEiOiJja3hxYnFhd3Q1MnVrMm5vY3hhNGtwaWg2In0.JhEvifYpx8vXyOf4vSq8ng';
   
-    var map = new mapboxgl.Map({
+    const map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
         center: [-122.436699, 37.809326],
@@ -15,20 +16,38 @@ function createMap() {
       });
     
 
-    
+    setMarkers(map);
       // TODO: add a marker to the map
-      
+      /*
       var marker = new mapboxgl.Marker()
       .setLngLat([-122.436699, 37.809326])
       .addTo(map);
-      
+      */
+
+
       }
     
 
-    window.onload = () => {
+window.onload = () => {
       createMap();
-      //move();
     };
+
+
+    async function setMarkers(map) {
+    // calls the collect function and then breaks it into outbound and inbound data arrays 
+    // var map = createMap();
+    console.log("Is this working?")
+      var BusObject = await collect();
+      OBarray = BusObject.Outbound;
+      IBarray = BusObject.Inbound;
+
+      OBarray.forEach((location) => {
+          console.log(location);
+        var marker = new mapboxgl.Marker()
+        .setLngLat([location.Longitude, location.Latitude])
+        .addTo(map);
+      });
+    }
 
     /* THIS IS THE RIGHT IDEA BUT SOMETHINGS WRONG
 // this function moves the busses on the map 
@@ -43,7 +62,7 @@ async function move(){
         .setLngLat([Number(location.Longitude), Number(location.Latitude)])
         .addTo(map);
     });
-    */
+
     marker.Marker().setLngLat([-122.436610, 37.899326]);
     //.addTo(map);
 }
@@ -57,10 +76,24 @@ async function collect(){
     rawLocations.forEach((route) => {
         if (route.MonitoredVehicleJourney.LineRef == selectedRoute) {
             if (route.MonitoredVehicleJourney.DirectionRef == "OB") {
-                RouteLocationsOB.push(route.MonitoredVehicleJourney.VehicleLocation)
+                RouteLocationsOB.push({
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [route.MonitoredVehicleJourney.VehicleLocation.Longitude, route.MonitoredVehicleJourney.VehicleLocation.Latitude]
+                    },
+                    "properties":{}
+                }); 
             }
             else if (route.MonitoredVehicleJourney.DirectionRef == "IB") {
-                RouteLocationsIB.push(route.MonitoredVehicleJourney.VehicleLocation)
+                RouteLocationsIB.push({
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [route.MonitoredVehicleJourney.VehicleLocation.Longitude, route.MonitoredVehicleJourney.VehicleLocation.Latitude]
+                    },
+                    "properties":{}
+                }); 
             }
         }
     });
