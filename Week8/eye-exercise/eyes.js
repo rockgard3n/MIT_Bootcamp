7f712e61eb9072cd94faea68c51ad3a1cc3ec9aa
rockgard3n <90724599+rockgard3n@users.noreply.github.com>
2022-01-10 11:47:27 -0800

    tweeks bruh

---
+++ Week8/eye-exercise/eyes.js
@@ -0,0 +1,14 @@
+// gets the ball attribute from DOM
+var balls = document.getElementsByClassName("ball");
+
+//tracks mouse movement and updates ball attributes to create a follow effect
+document.onmousemove = () => {
+  var x = (event.clientX * 100) / window.innerWidth + "%";
+  var y = (event.clientY * 100) / window.innerHeight + "%";
+
+  for (let i = 0; i < 2; i++) {
+    balls[i].style.left = x;
+    balls[i].style.top = y;
+    balls[i].transfoorm = "translate(-" + x + ",-" + y + ")";
+  }
+};
