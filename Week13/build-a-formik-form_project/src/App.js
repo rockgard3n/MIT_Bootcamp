119f041e2da7d696929d8cb874c81834b8362ab8
rockgard3n <90724599+rockgard3n@users.noreply.github.com>
2022-01-31 17:55:29 -0800

    Created a formik form

--- Week13/build-a-formik-form_project/src/App.js
+++ Week13/build-a-formik-form_project/src/App.js
@@ -1,15 +1,44 @@
-import React from "react";
-// TODO: import useFormik from formik library
+import React from 'react';
+import { useFormik} from 'formik'
 
 function App() {
-  // TODO: add a const called formik assigned to useFormik()
-
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      emailField: '',
+      pswField: ''      
+    },
+    onSubmit: values =>{
+      alert("Login Succesful");
+      console.log('form:',values);
+    },
+    validate: values =>{
+      let errors = {};
+      if(!values.name) errors.name = 'field required';
+      if (!values.emailField) {
+        errors.emailField = 'field required';
+      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.emailField)) {
+        errors.emailField = 'username should be an email address';
+      };
+      if(!values.pswField) errors.pswField = 'field required';
+      return errors;
+    }
+  });
+  
   return (
     <div>
-      <p>
-        The app is ready! You can proceed with the task instructions. TODO:
-        build you form here.
-      </p>
+      <form onSubmit={formik.handleSubmit}>
+        <div>Name:</div>
+        <input type="text" name="name" onChange={formik.handleChange} value={formik.values.name}/>
+        {formik.errors.name ? <div style={{color:'red'}}>{formik.errors.name}</div> : null}
+        <div>Email:</div>
+        <input type="text" name="emailField" onChange={formik.handleChange} value={formik.values.email}/>
+        {formik.errors.emailField ? <div style={{color:'red'}}>{formik.errors.emailField}</div> : null}        
+        <div>Password:</div>
+        <input type="text" name="pswField" onChange={formik.handleChange} value={formik.values.pswField}/><br/>
+        {formik.errors.pswField ? <div style={{color:'red'}}>{formik.errors.pswField}</div> : null}                
+        <button name="suubmitBtn" type="submit">Submit</button>
+      </form>      
     </div>
   );
 }
