edbe129d7399b835aa7f1152f2a562bd2a288e33
rockgard3n <90724599+rockgard3n@users.noreply.github.com>
2022-02-01 17:01:15 -0800

    Working on lists
    my workshop file i made from scratch (ish) is called MyAttempts

--- Week14/ToDo-List/Styling-ToDo-List/index.js
+++ Week14/ToDo-List/Styling-ToDo-List/index.js
@@ -13,12 +13,37 @@ function App(){
       isCompleted: false,
     }        
   ])
-
+  // define variable and state to use for adding todos
+  const [value, setValue] = React.useState('');
+  // function to handle the input on an event
+  const handleSubmit = e => {
+    // prevents the autoreloud of page
+    e.preventDefault();
+    // if there is an empty value do nothing
+    if (!value) return;
+    // destructures the list, then adds our new value
+    const newTodos = [...todos, {text: value, isCompleted: false}];
+    // sets it to the new list we just made
+    setTodos(newTodos);
+    //resets it to empty after we're done with our shiz 
+    setValue('');
+  }
+  const removeTodo = e => {
+    //get handle on id that targets div we want
+    const index = Number(e.target.id);
+    let temp = [...todos];
+    temp.splice(index, 1);
+    setTodos(temp);
+  }
   return(
     <>
       {todos.map((todo, i) => (
-        <div className="todo" key={i}>{todo.text}</div>
+        // the classname helps us target the div, the id and the onclick we use to remove it
+        <div className="todo" key={i} id={i} onClick={removeTodo}>{todo.text}</div>
       ))}
+      <form onSubmit={handleSubmit}>
+        <input type="text" className="input" value={value} placeholder="Add ToDo .." onChange={e => setValue(e.target.value)}/>
+      </form>
     </>
   );
 }
