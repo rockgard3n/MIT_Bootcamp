03572b3a8743ea94c9c0d5402bc837a83e86afdb
rockgard3n <90724599+rockgard3n@users.noreply.github.com>
2022-03-22 17:52:23 -0700

    Update cart.jsx

--- Week19/shopping-cart-master/cart.jsx
+++ Week19/shopping-cart-master/cart.jsx
@@ -1,4 +1,4 @@
-// LIAM: simulate getting products from DataBase
+// LIAM: gets our baseline before we call the DB
 const products = [
   { name: "Apples", country: "Italy", cost: 3, instock: 10 },
   { name: "Oranges", country: "Spain", cost: 4, instock: 3 },
