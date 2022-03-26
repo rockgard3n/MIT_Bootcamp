9644399f2e94b7cce365e50b3f01055723189a80
rockgard3n <liamcashel@gmail.com>
2022-03-25 21:35:37 -0700

    added strapi ap

--- Week20/restaurants/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'fd0ed6fa21e6f6669a01b72f392ed16f'),
-    },
-  },
-});
